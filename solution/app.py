4187e867077108dd3e4dc58652d661f2b0b72def
Mikhail Samylov <Samylov-Mikhail@yandex.com>
2024-02-27 20:30:48 +0300

    chore: use application factory pattern

--- solution/app.py
+++ solution/app.py
@@ -1,10 +1,14 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 
-app = Flask(__name__)
 
-@app.route('/api/ping', methods=['GET'])
-def send():
+def ping():
     return jsonify({"status": "ok"}), 200
 
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    app.add_url_rule("/api/ping", view_func=ping, methods=["GET"])
+    return app
+
 if __name__ == "__main__":
-    app.run()
+    create_app().run()

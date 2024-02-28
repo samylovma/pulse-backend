e14cd80fc92f6bac68e2d43e3292fad70096aa56
Mikhail Samylov <Samylov-Mikhail@yandex.com>
2024-02-28 23:46:15 +0300

    feat: implement `02/countries`

--- solution/src/pulse_backend/main.py
+++ solution/src/pulse_backend/main.py
@@ -2,7 +2,7 @@ from os import getenv
 from typing import Literal
 
 import pydantic
-from litestar import Litestar, MediaType, Router, get
+from litestar import Litestar, MediaType, Response, Router, get, status_codes
 from litestar.contrib.sqlalchemy.base import CommonTableAttributes
 from litestar.contrib.sqlalchemy.plugins import (
     SQLAlchemyAsyncConfig,
@@ -11,23 +11,10 @@ from litestar.contrib.sqlalchemy.plugins import (
 from litestar.contrib.sqlalchemy.repository import SQLAlchemyAsyncRepository
 from litestar.controller import Controller
 from litestar.di import Provide
-from sqlalchemy import TEXT, URL, select
+from litestar.openapi import ResponseSpec
+from sqlalchemy import TEXT, URL
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-
-
-class BaseORM(DeclarativeBase):
-    ...
-
-
-class CountryORM(CommonTableAttributes, BaseORM):
-    __tablename__ = "countries"  # type: ignore[assignment]
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(TEXT())
-    alpha2: Mapped[str] = mapped_column(TEXT())
-    alpha3: Mapped[str] = mapped_column(TEXT())
-    region: Mapped[str] = mapped_column(TEXT())
 
 
 class Country(pydantic.BaseModel):
@@ -39,8 +26,26 @@ class Country(pydantic.BaseModel):
     region: str
 
 
-class CountryRepository(SQLAlchemyAsyncRepository[CountryORM]):
-    model_type = CountryORM
+class ErrorResponse(pydantic.BaseModel):
+    reason: str
+
+
+class BaseModel(DeclarativeBase):
+    ...
+
+
+class CountryModel(CommonTableAttributes, BaseModel):
+    __tablename__ = "countries"  # type: ignore[assignment]
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(TEXT())
+    alpha2: Mapped[str] = mapped_column(TEXT())
+    alpha3: Mapped[str] = mapped_column(TEXT())
+    region: Mapped[str] = mapped_column(TEXT())
+
+
+class CountryRepository(SQLAlchemyAsyncRepository[CountryModel]):
+    model_type = CountryModel
 
 
 async def provide_country_repo(db_session: AsyncSession) -> CountryRepository:
@@ -57,16 +62,38 @@ class CountryController(Controller):
     dependencies = {"country_repo": Provide(provide_country_repo)}
 
     @get()
-    async def countries(
-        self, country_repo: CountryRepository, region: str | None = None
+    async def list_countries(
+        self, country_repo: CountryRepository, region: list[str] | None = None
     ) -> list[Country]:
-        if region:
-            countries = await country_repo.list(
-                statement=select(CountryORM).where(CountryORM.region == region)
-            )
-        else:
+        if region is None:
             countries = await country_repo.list()
+        else:
+            countries = await country_repo.list(
+                statement=country_repo.statement.where(
+                    country_repo.model_type.region.in_(region)
+                )
+            )
         return [Country.model_validate(country) for country in countries]
+
+    @get(
+        "/{alpha2:str}",
+        responses={
+            404: ResponseSpec(
+                data_container=ErrorResponse, description="Country not found"
+            ),
+        },
+    )
+    async def get_country(
+        self, country_repo: CountryRepository, alpha2: str
+    ) -> Country | Response[ErrorResponse]:
+        country = await country_repo.get_one_or_none(alpha2=alpha2)
+        if country is not None:
+            return Country.model_validate(country)
+        else:
+            return Response(
+                content=ErrorResponse(reason=f'Country "{alpha2}" not found.'),
+                status_code=status_codes.HTTP_404_NOT_FOUND,
+            )
 
 
 def create_app() -> Litestar:

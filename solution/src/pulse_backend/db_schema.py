7c46c7fd8bc92388e0c23721421894ee625c9e6e
Mikhail Samylov <Samylov-Mikhail@yandex.com>
2024-03-01 21:03:04 +0300

    chore: add `alembic`; add `User` db model

--- solution/src/pulse_backend/db_schema.py
+++ solution/src/pulse_backend/db_schema.py
@@ -1,6 +1,6 @@
 from advanced_alchemy.base import CommonTableAttributes
-from sqlalchemy import TEXT
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import TEXT, ForeignKey, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
 class Base(DeclarativeBase):
@@ -12,6 +12,20 @@ class Country(CommonTableAttributes, Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(TEXT())
-    alpha2: Mapped[str] = mapped_column(TEXT())
+    alpha2: Mapped[str] = mapped_column(TEXT(), unique=True)
     alpha3: Mapped[str] = mapped_column(TEXT())
     region: Mapped[str] = mapped_column(TEXT())
+
+
+class User(CommonTableAttributes, Base):
+    login: Mapped[str] = mapped_column(
+        String(30), primary_key=True, unique=True
+    )
+    email: Mapped[str] = mapped_column(String(50), unique=True)
+    password: Mapped[bytes]
+    country_code: Mapped[str] = mapped_column(ForeignKey(Country.alpha2))
+    is_public: Mapped[bool]
+    phone: Mapped[str | None] = mapped_column(String(20))
+    image: Mapped[str | None] = mapped_column(String(200))
+
+    country: Mapped[Country] = relationship()

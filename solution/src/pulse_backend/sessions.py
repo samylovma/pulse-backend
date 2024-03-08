7fcf7ea9cb41a0ec85abfdadbbca95c23491b5e1
Mikhail Samylov <Samylov-Mikhail@yandex.com>
2024-03-08 16:45:12 +0300

    refactor: `sessions` module

--- solution/src/pulse_backend/sessions.py
+++ solution/src/pulse_backend/sessions.py
@@ -1,10 +1,10 @@
 from collections.abc import Sequence
-from dataclasses import dataclass
 from os import getenv
 from typing import Any
 
 from jose import jwt
 from jose.exceptions import ExpiredSignatureError, JWTClaimsError, JWTError
+from litestar.config.app import AppConfig
 from litestar.connection import ASGIConnection
 from litestar.exceptions import NotAuthorizedException
 from litestar.middleware import DefineMiddleware
@@ -12,15 +12,9 @@ from litestar.middleware.authentication import (
     AbstractAuthenticationMiddleware,
     AuthenticationResult,
 )
-from litestar.openapi.spec import (
-    Components,
-    SecurityRequirement,
-    SecurityScheme,
-)
-from litestar.security.base import AbstractSecurityConfig
 from litestar.types import ASGIApp, Method, Scopes
 
-from pulse_backend.db.models import Session, User
+from pulse_backend.db.models import Session
 from pulse_backend.dependencies import provide_session_service
 
 
@@ -46,11 +40,6 @@ class JWTSessionAuthenticationMiddleware(AbstractAuthenticationMiddleware):
     async def authenticate_request(
         self, connection: ASGIConnection[Any, Any, Any, Any]
     ) -> AuthenticationResult:
-        db_session = await connection.app.dependencies["db_session"](
-            state=connection.app.state, scope=connection.scope
-        )
-        session_service = await provide_session_service(db_session)
-
         auth_header = connection.headers.get("Authorization")
         if auth_header is None:
             raise NotAuthorizedException('No "Authorization" header')
@@ -58,6 +47,7 @@ class JWTSessionAuthenticationMiddleware(AbstractAuthenticationMiddleware):
             raise NotAuthorizedException(
                 "The authentication scheme is not supported"
             )
+
         try:
             claims = jwt.decode(
                 token=auth_header[7:],
@@ -67,48 +57,47 @@ class JWTSessionAuthenticationMiddleware(AbstractAuthenticationMiddleware):
             )
         except (JWTError, ExpiredSignatureError, JWTClaimsError) as e:
             raise NotAuthorizedException("Invalid token") from e
+
+        db_session = await connection.app.dependencies["db_session"](
+            state=connection.app.state, scope=connection.scope
+        )
+        session_service = await provide_session_service(db_session)
         session = await session_service.get_one_or_none(id=claims["jti"])
         if session is None:
             raise NotAuthorizedException("Invalid token")
+
         return AuthenticationResult(user=session.user, auth=session)
 
 
-@dataclass
-class JWTSessionAuthentication(AbstractSecurityConfig[User, Session]):
-    token_secret: str
-    exclude: str | list[str] | None = None
-    exclude_http_methods: Sequence[Method] | None = ("OPTIONS", "HEAD")
-
-    def __post_init__(self) -> None:
-        pass
-
-    @property
-    def openapi_components(self) -> Components:
-        return Components(
-            security_schemes={
-                "BearerToken": SecurityScheme(
-                    type="http",
-                    name="Authorization",
-                    scheme="Bearer",
-                    bearer_format="JWT",
-                )
-            }
-        )
-
-    @property
-    def security_requirement(self) -> SecurityRequirement:
-        return {"BearerToken": []}
+class JWTSessionAuthentication:
+    def __init__(
+        self,
+        token_secret: str,
+        exclude: str | list[str] | None = None,
+        exclude_from_auth_key: str = "exclude_from_auth",
+        exclude_http_methods: Sequence[Method] | None = ("OPTIONS", "HEAD"),
+        scopes: Scopes | None = None,
+    ) -> None:
+        self.token_secret = token_secret
+        self.exclude = exclude
+        self.exclude_from_auth_key = exclude_from_auth_key
+        self.exclude_http_methods = exclude_http_methods
+        self.scopes = scopes
 
     @property
     def middleware(self) -> DefineMiddleware:
         return DefineMiddleware(
             JWTSessionAuthenticationMiddleware,
             exclude=self.exclude,
-            exclude_from_auth_key=self.exclude_opt_key,
+            exclude_from_auth_key=self.exclude_from_auth_key,
             exclude_http_methods=self.exclude_http_methods,
             scopes=self.scopes,
             token_secret=self.token_secret,
         )
+
+    def on_app_init(self, app_config: AppConfig) -> AppConfig:
+        app_config.middleware.insert(0, self.middleware)
+        return app_config
 
     def create_token(self, session: Session) -> str:
         token = jwt.encode(

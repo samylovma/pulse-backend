8da712b56f80799afc9cb511d262ec89e8563dc5
Mikhail Samylov <Samylov-Mikhail@yandex.com>
2024-03-12 00:04:00 +0300

    test: use `AsyncTestClient`

--- solution/tests/api/test_ping.py
+++ solution/tests/api/test_ping.py
@@ -1,11 +1,11 @@
 from litestar.status_codes import HTTP_200_OK
-from litestar.testing import create_test_client
+from litestar.testing import AsyncTestClient
 
-from pulse_backend.api.ping import ping
+from pulse_backend.app import create_app
 
 
-def test_ping():
-    with create_test_client(ping) as client:
-        response = client.get("/api/ping")
+async def test_ping() -> None:
+    async with AsyncTestClient(create_app()) as client:
+        response = await client.get("/api/ping")
         assert response.status_code == HTTP_200_OK
         assert response.text == "ok"
